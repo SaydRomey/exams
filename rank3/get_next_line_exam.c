9f885f3c4c5c2fb560f83f9f53df53db7158212b
Sayd Romey <sayd@null.net>
2023-12-14 09:20:56 -0500

    gnl

--- rank3/get_next_line_exam.c
+++ rank3/get_next_line_exam.c
@@ -1,45 +1,94 @@
 
-// Gnl exam version?
+ #include <unistd.h> 
+ #include <stdlib.h> 
 
-#ifndef GET_NEXT_LINE_H 
- # define GET_NEXT_LINE_H 
-  
- # include <unistd.h> 
- # include <stdlib.h> 
-  
- # ifndef BUFFER_SIZE 
- #  define BUFFER_SIZE 42 
- # endif 
-  
- char        *get_next_line(int fd); 
-  
+ #ifndef BUFFER_SIZE 
+ # define BUFFER_SIZE 42 
  #endif
 
-…
-
-#include "get_next_line.h" 
+// char *get_next_line(int fd) 
+// { 
+// 	int		i = 0; 
+// 	int		rd = 0; 
+// 	char	character; 
+// 	char	*buffer = malloc(10000); 
   
- char *get_next_line(int fd) 
- { 
-     int     i = 0; 
-     int     rd = 0; 
-     char    character; 
-     char     *buffer = malloc(10000); 
-  
-     if (BUFFER_SIZE <= 0) 
-         return (free(buffer), NULL); 
-     while ((rd = read(fd, &character, BUFFER_SIZE - BUFFER_SIZE + 1)) > 0) 
-     { 
-         buffer[i++] = character; 
-         if (character == '\n') 
-             break; 
-     } 
-     if ((!buffer[i - 1] && !rd) || rd == -1) 
-     { 
-         free(buffer); 
-         return (NULL); 
-     } 
-     buffer[i] =  '\0'; 
-     return(buffer); 
- }
+// 	if (BUFFER_SIZE <= 0) 
+// 		return (free(buffer), NULL); 
+// 	while ((rd = read(fd, &character, BUFFER_SIZE - BUFFER_SIZE + 1)) > 0) 
+// 	{ 
+// 		buffer[i++] = character; 
+// 		if (character == '\n') 
+// 			break; 
+// 	} 
+// 	if ((!buffer[i - 1] && !rd) || rd == -1) 
+// 	{ 
+// 		free(buffer); 
+// 		return (NULL); 
+// 	} 
+// 	buffer[i] =  '\0'; 
+// 	return(buffer); 
+// }
 
+// 
+
+char	*get_next_line(int fd)
+{
+	int		index;
+	int		bytes;
+	char	*buffer;
+	char	character;
+
+	if ((fd < 0) || (BUFFER_SIZE <= 0))
+		return (NULL);
+	index = 0;
+	bytes = read(fd, &character, 1);
+	buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	while (bytes > 0)
+	{
+		buffer[index++] = character;
+		if (character == '\n')
+			break ;
+		bytes = read(fd, &character, 1);
+	}
+	if ((bytes <= 0) && (index == 0))
+		return (free(buffer), NULL);
+	buffer[index] = '\0';
+	return (buffer);
+}
+
+/* -------------------------------------------------------------------------- */
+
+#include <stdio.h>
+#include <fcntl.h>
+
+int	main(int argc, char **argv)
+{
+	int		fd;
+	char	*line;
+	int		line_nbr = 1;
+	
+	if (argc != 2)
+	{
+		printf("Usage: %s <filename>\n", argv[0]);
+		return (0);
+	}
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		printf("Error: could not open file %s\n", argv[1]);
+		return (0);
+	}
+	line = get_next_line(fd);
+	while (line)
+	{
+		printf("[%d] %s\n", line_nbr, line);
+		free(line);
+		line = get_next_line(fd);
+		line_nbr++;
+	}
+	free(line);
+	close(fd);
+	return (0);
+}
+// EOF
